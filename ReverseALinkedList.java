b39ce7051b780fba56d6017f74375e19474d29b8
SHAVUKARI SANDEEP <sandy20499@gmail.com>
2025-03-11 10:50:53 +0530

    Create ReverseALinkedList.java

---
+++ ReverseALinkedList.java
@@ -0,0 +1,42 @@
+// function Template for Java
+
+/* linked list node class:
+
+class Node {
+    int data;
+    Node next;
+    Node(int value) {
+        this.value = value;
+    }
+}
+
+*/
+
+class ReverseALinkedList {
+    Node reverseList(Node head) {
+        // code here
+        Stack<Integer> s= new Stack<>();
+        while(head!=null)
+        {
+            s.push(head.data);
+            head=head.next;
+        }
+        Node new_node=null;
+        Node temp=null;
+        while(!s.isEmpty())
+        {
+            if(new_node==null)
+            {
+                new_node= new Node(s.pop());
+                temp=new_node;
+            }
+            else
+            {
+                temp.next= new Node(s.pop());
+                temp=temp.next;
+            }
+        }
+    
+        return new_node;    
+    }
+}
