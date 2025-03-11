2068fe436541684d8cbfe3f21187e9f3e6b69d1b
SHAVUKARI SANDEEP <sandy20499@gmail.com>
2025-03-11 17:22:31 +0530

    Create CheckIfLinkedListIsPalindrome.java

---
+++ CheckIfLinkedListIsPalindrome.java
@@ -0,0 +1,37 @@
+public class CheckIfLinkedListIsPalindrome {
+    static boolean isPalindrome(Node head) {
+        // Your code here
+        Stack<Integer> s= new Stack<>();
+        Node temp= head;
+        // Node cmp=head;
+        Node new_node=null;
+        while(temp!=null)
+        {
+            s.push(temp.data);
+            temp=temp.next;
+        }
+        while(!s.isEmpty())
+        {
+            if(new_node==null)
+            {
+                new_node = new Node(s.pop());
+                temp=new_node;
+            }
+            else
+            {
+                temp.next= new Node(s.pop());
+                temp=temp.next;
+            }
+        }
+        while(head!=null&&new_node!= null)
+        {
+            if(head.data!=new_node.data)
+            {
+                return false;
+            }
+            head=head.next;
+            new_node= new_node.next;
+        }
+        return true;
+    }
+}
