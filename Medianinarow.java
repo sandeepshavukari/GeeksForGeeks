6fb0d7a442173f5b8bff24c99d27eb91c4b684c0
sandeepshavukari <sandy20499@gmail.com>
2025-08-19 07:53:57 +0530

    MedianinarowwisesortedMatrix

---
+++ Medianinarow.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Medianinarow-wisesortedMatrix {
+    public int median(int[][] mat) {
+        // code here
+        int n=mat.length;
+        int m=mat[0].length;
+        int ar[]= new int [n*m];
+        int k=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                ar[k++]=mat[i][j];
+            }
+        }
+        Arrays.sort(ar);
+        if(ar.length%2!=0)
+        return ar[ar.length/2];
+        else
+        return (ar[ar.length/2]+ar[(ar.length/2)+1])/2;
+    }
+}
